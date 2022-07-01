069bc73efcecb203e0a9d3a1f6277fa3cc149a00
Chimoha31 <mokochii1108@gmail.com>
2022-07-01 13:43:05 -0700

    Added autoComplete

--- server/client/src/components/auth/Login.jsx
+++ server/client/src/components/auth/Login.jsx
@@ -45,6 +45,8 @@ const Login = () => {
           <form onSubmit={handleSubmit}>
             <input
               type="email"
+              name="email"
+              autocomplete="username"
               placeholder="Email address"
               className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
               onChange={(e) => setEmail(e.target.value)}
--- server/client/src/components/auth/SignUp.jsx
+++ server/client/src/components/auth/SignUp.jsx
@@ -34,6 +34,8 @@ const SignUp = () => {
           <form onSubmit={handleSubmit}>
             <input
               type="email"
+              name="email"
+              autocomplete="username"
               placeholder="Email address"
               className="block w-72 border border-stone-700 px-3 py-2 rounded-md mb-3"
               onChange={(e) => setEmail(e.target.value)}
